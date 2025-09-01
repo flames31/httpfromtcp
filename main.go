3b5de283820bdb0d830a05d4bf9a1030fed37fa6
flames31 <rrahulao2000@gmail.com>
2025-09-01 12:30:00 -0400

    C1L3

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Printf("error opening file : %v", err)
+		os.Exit(1)
+	}
+
+	bytes := make([]byte, 8)
+	for {
+		n, err := file.Read(bytes)
+		if err != nil {
+			break
+		}
+		fmt.Printf("read: %s\n", string(bytes[:n]))
+	}
 }
